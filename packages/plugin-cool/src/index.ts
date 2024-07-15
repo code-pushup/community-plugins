09ebf752456e242e03e7182c22c1bb88684e2356
Michael <michael.hladky@push-based.io>
2024-07-15 14:50:19 +0200

    wip

--- packages/plugin-cool/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { pluginCoolPlugin } from './lib/plugin-cool-plugin';
-
-export default pluginCoolPlugin;
-export { pluginCoolPlugin } from './lib/plugin-cool-plugin';
-export type { PluginCoolPluginConfig } from './lib/plugin-cool-plugin';
