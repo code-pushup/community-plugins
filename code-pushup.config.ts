1450eb6e8b63e7a6f9763757c3cc82988ce096ae
Michael <michael.hladky@push-based.io>
2024-07-18 21:30:11 +0200

    wip

--- code-pushup.config.ts
+++
@@ -1,5 +0,0 @@
-import bundleSizePlugin from './dist/packages/bundle-size';
-import { fileSizePlugin } from './dist/packages/file-size';
-export default {
-  plugins: [bundleSizePlugin(), fileSizePlugin()],
-};
