a218f3aab3c9ff2f7f9d4c07b95d97e00a89791a
Michael <michael.hladky@push-based.io>
2024-07-18 22:15:24 +0200

    remove dummy projects

--- packages/bundle-size/src/index.ts
+++
@@ -1,5 +0,0 @@
-import bundleSizePlugin from './lib/bundle-size-plugin';
-
-export default bundleSizePlugin;
-export { bundleSizePlugin } from './lib/bundle-size-plugin';
-export { BundleSizePluginOptions } from './lib/bundle-size-plugin';
