373fb83a7a5a06a35f76ff92f20c774c80b6b85f
Michael Hladky <michael.hladky@push-based.io>
2025-12-08 19:08:28 +0100

    refactor: wip

--- e2e/plugin-knip-e2e/tests/knip-reporter.e2e.test.ts
+++ e2e/plugin-knip-e2e/tests/knip-reporter.e2e.test.ts
@@ -1,77 +1,81 @@
-import { cp } from 'node:fs/promises';
+import {cp} from 'node:fs/promises';
 import path from 'node:path';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
-import { auditOutputsSchema } from '@code-pushup/models';
-import { nxTargetProject } from '@code-pushup/test-nx-utils';
+import {afterAll, beforeAll, describe, expect, it} from 'vitest';
+import {auditOutputsSchema} from '@code-pushup/models';
+import {nxTargetProject} from '@code-pushup/test-nx-utils';
 import {
-  E2E_ENVIRONMENTS_DIR,
-  TEST_OUTPUT_DIR,
-  restoreNxIgnoredFiles,
-  teardownTestFolder,
+    E2E_ENVIRONMENTS_DIR,
+    TEST_OUTPUT_DIR,
+    restoreNxIgnoredFiles,
+    teardownTestFolder,
 } from '@code-pushup/test-utils';
-import { executeProcess, readJsonFile } from '@code-pushup/utils';
+import {executeProcess, readJsonFile} from '@code-pushup/utils';
 
 describe('knip reporter for code pushup audits', () => {
-  const envDir = path.join(E2E_ENVIRONMENTS_DIR, nxTargetProject());
-  const testFileDir = path.join(envDir, TEST_OUTPUT_DIR, 'reporter');
-  const reporterSetupDir = path.join(testFileDir, 'reporter-setup');
-  const fixturesDir = path.join('e2e', nxTargetProject(), 'mocks/fixtures');
-  const reporterPath = path.resolve(
-    envDir,
-    'node_modules',
-    '@code-pushup',
-    'knip-plugin',
-    'src',
-    'lib',
-    'reporter.js',
-  );
+    const envDir = path.join(E2E_ENVIRONMENTS_DIR, nxTargetProject());
+    const testFileDir = path.join(envDir, TEST_OUTPUT_DIR, 'reporter');
+    const reporterSetupDir = path.join(testFileDir, 'reporter-setup');
+    const fixturesDir = path.join('e2e', nxTargetProject(), 'mocks/fixtures');
+    const reporterPath = path.resolve(
+        envDir,
+        'node_modules',
+        '@code-pushup',
+        'knip-plugin',
+        'src',
+        'lib',
+        'reporter.js',
+    );
 
-  beforeAll(async () => {
-    await cp(fixturesDir, testFileDir, { recursive: true });
-    await restoreNxIgnoredFiles(testFileDir);
-  });
-
-  afterAll(async () => {
-    await teardownTestFolder(testFileDir);
-  });
-
-  it('should execute knip with custom reporter and generate report', async () => {
-    const outputFile = path.join(reporterSetupDir, 'knip-report.json');
-
-    const { code } = await executeProcess({
-      command: 'npx',
-      args: [
-        'knip',
-        '--no-exit-code',
-        `--reporter=${reporterPath}`,
-        `--reporter-options='{\"outputFile\":\"knip-report.json\"}'`,
-      ],
-      cwd: reporterSetupDir,
+    beforeAll(async () => {
+        await cp(fixturesDir, testFileDir, {recursive: true});
+        await restoreNxIgnoredFiles(testFileDir);
     });
 
-    expect(code).toBe(0);
+    afterAll(async () => {
+        await teardownTestFolder(testFileDir);
+    });
 
-    const report = await readJsonFile(outputFile);
-    expect(() => auditOutputsSchema.parse(report)).not.toThrowError();
-    expect(report).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          slug: 'dependencies',
-          score: 0,
-          value: 1,
-          details: {
-            issues: [
-              {
-                message: 'Unused dependency zod',
-                severity: 'error',
-                source: {
-                  file: 'package.json',
-                },
-              },
+    it('should execute knip with custom reporter and generate report', async () => {
+        const outputFile = path.join(reporterSetupDir, 'knip-report.json');
+
+        const {code} = await executeProcess({
+            command: 'npx',
+            args: [
+                'knip',
+                '--no-exit-code',
+                `--reporter=${reporterPath}`,
+                `--reporter-options='{\"outputFile\":\"knip-report.json\"}'`,
             ],
-          },
-        }),
-      ]),
-    );
-  });
+            cwd: reporterSetupDir,
+        });
+
+        expect(code).toBe(0);
+
+        const report = await readJsonFile(outputFile);
+        expect(() => auditOutputsSchema.parse(report)).not.toThrowError();
+        expect(report).toStrictEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    slug: 'dependencies',
+                    score: 0,
+                    value: 1,
+                    details: {
+                        issues: [
+                            {
+                                message: 'Unused dependency zod',
+                                severity: 'error',
+                                source: {
+                                    "file": "tmp/e2e/plugin-knip-e2e/__test__/reporter/reporter-setup/package.json",
+                                    "position": {
+                                        "startColumn": 6,
+                                        "startLine": 4,
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                }),
+            ]),
+        );
+    });
 });
